2311e90cd2a8736e7ad9e6b57fb0158565c8348b
Sergey Makeev <sergeymakeev@hotmail.com>
2025-07-02 09:04:42 -0700

    switch from <int> to <TestObject> for perf testing

--- performance_test.cpp
+++ performance_test.cpp
@@ -8,6 +8,88 @@
 
 using namespace dod;
 
+// Custom test object with non-trivial constructor/destructor for fair comparison
+class TestObject {
+private:
+    int value_;
+    static size_t construction_count_;
+    static size_t destruction_count_;
+    
+public:
+    TestObject() : value_(0) {
+        ++construction_count_;
+    }
+    
+    TestObject(int val) : value_(val) {
+        ++construction_count_;
+    }
+    
+    TestObject(size_t val) : value_(static_cast<int>(val)) {
+        ++construction_count_;
+    }
+    
+    TestObject(const TestObject& other) : value_(other.value_) {
+        ++construction_count_;
+    }
+    
+    TestObject& operator=(const TestObject& other) {
+        if (this != &other) {
+            value_ = other.value_;
+        }
+        return *this;
+    }
+    
+    TestObject& operator=(int val) {
+        value_ = val;
+        return *this;
+    }
+    
+    TestObject& operator=(size_t val) {
+        value_ = static_cast<int>(val);
+        return *this;
+    }
+    
+    ~TestObject() {
+        ++destruction_count_;
+    }
+    
+    // Conversion operators and arithmetic for compatibility with tests
+    operator int() const { return value_; }
+    
+    TestObject operator+(const TestObject& other) const {
+        return TestObject(value_ + other.value_);
+    }
+    
+    TestObject& operator+=(const TestObject& other) {
+        value_ += other.value_;
+        return *this;
+    }
+    
+    // Volatile-compatible addition operator for benchmarking
+    TestObject& operator+=(const TestObject& other) volatile {
+        const_cast<TestObject*>(this)->value_ += other.value_;
+        return const_cast<TestObject&>(*this);
+    }
+    
+    bool operator==(const TestObject& other) const {
+        return value_ == other.value_;
+    }
+    
+    bool operator!=(const TestObject& other) const {
+        return value_ != other.value_;
+    }
+    
+    int get_value() const { return value_; }
+    
+    static size_t get_construction_count() { return construction_count_; }
+    static size_t get_destruction_count() { return destruction_count_; }
+    static void reset_counters() { construction_count_ = 0; destruction_count_ = 0; }
+};
+
+// Static member definitions
+size_t TestObject::construction_count_ = 0;
+size_t TestObject::destruction_count_ = 0;
+
 // Test data sizes
 constexpr size_t SMALL_SIZE = 1000;
 constexpr size_t MEDIUM_SIZE = 100000;
@@ -27,7 +109,7 @@ template<typename Container>
 void test_push_back(size_t size) {
     Container vec;
     for (size_t i = 0; i < size; ++i) {
-        vec.push_back(static_cast<typename Container::value_type>(i));
+        vec.push_back(typename Container::value_type(i));
     }
     do_not_optimize(vec);
 }
@@ -37,10 +119,10 @@ void test_sequential_access() {
     Container vec;
     vec.resize(MEDIUM_SIZE);
     for (size_t i = 0; i < MEDIUM_SIZE; ++i) {
-        vec[i] = static_cast<typename Container::value_type>(i);
+        vec[i] = typename Container::value_type(i);
     }
     
-    volatile typename Container::value_type sum = 0;
+    volatile typename Container::value_type sum = typename Container::value_type(0);
     for (size_t i = 0; i < vec.size(); ++i) {
         sum += vec[i];
     }
@@ -52,13 +134,13 @@ void test_random_access() {
     Container vec;
     vec.resize(MEDIUM_SIZE);
     for (size_t i = 0; i < MEDIUM_SIZE; ++i) {
-        vec[i] = static_cast<typename Container::value_type>(i);
+        vec[i] = typename Container::value_type(i);
     }
     
     std::mt19937 gen(42);
     std::uniform_int_distribution<size_t> dis(0, vec.size() - 1);
     
-    volatile typename Container::value_type sum = 0;
+    volatile typename Container::value_type sum = typename Container::value_type(0);
     for (int i = 0; i < 10000; ++i) {
         size_t idx = dis(gen);
         sum += vec[idx];
@@ -71,10 +153,10 @@ void test_iterator_traversal() {
     Container vec;
     vec.resize(MEDIUM_SIZE);
     for (size_t i = 0; i < MEDIUM_SIZE; ++i) {
-        vec[i] = static_cast<typename Container::value_type>(i);
+        vec[i] = typename Container::value_type(i);
     }
     
-    volatile typename Container::value_type sum = 0;
+    volatile typename Container::value_type sum = typename Container::value_type(0);
     for (auto it = vec.begin(); it != vec.end(); ++it) {
         sum += *it;
     }
@@ -86,10 +168,10 @@ void test_range_based_loop() {
     Container vec;
     vec.resize(MEDIUM_SIZE);
     for (size_t i = 0; i < MEDIUM_SIZE; ++i) {
-        vec[i] = static_cast<typename Container::value_type>(i);
+        vec[i] = typename Container::value_type(i);
     }
     
-    volatile typename Container::value_type sum = 0;
+    volatile typename Container::value_type sum = typename Container::value_type(0);
     for (const auto& value : vec) {
         sum += value;
     }
@@ -101,14 +183,14 @@ void test_reserve_performance() {
     Container vec;
     vec.reserve(LARGE_SIZE);
     for (size_t i = 0; i < LARGE_SIZE; ++i) {
-        vec.push_back(static_cast<typename Container::value_type>(i));
+        vec.push_back(typename Container::value_type(i));
     }
     do_not_optimize(vec);
 }
 
 template<typename Container>
 void test_construct_with_size() {
-    Container vec(MEDIUM_SIZE, static_cast<typename Container::value_type>(42));
+    Container vec(MEDIUM_SIZE, typename Container::value_type(42));
     do_not_optimize(vec);
 }
 
@@ -117,7 +199,7 @@ void test_construct_and_fill() {
     Container vec;
     vec.resize(MEDIUM_SIZE);
     for (size_t i = 0; i < vec.size(); ++i) {
-        vec[i] = static_cast<typename Container::value_type>(i * 2);
+        vec[i] = typename Container::value_type(i * 2);
     }
     do_not_optimize(vec);
 }
@@ -127,7 +209,7 @@ void test_copy_constructor() {
     Container original;
     original.resize(MEDIUM_SIZE);
     for (size_t i = 0; i < MEDIUM_SIZE; ++i) {
-        original[i] = static_cast<typename Container::value_type>(i);
+        original[i] = typename Container::value_type(i);
     }
     
     Container copy(original);
@@ -139,7 +221,7 @@ void test_copy_assignment() {
     Container original;
     original.resize(MEDIUM_SIZE);
     for (size_t i = 0; i < MEDIUM_SIZE; ++i) {
-        original[i] = static_cast<typename Container::value_type>(i);
+        original[i] = typename Container::value_type(i);
     }
     
     Container copy;
@@ -149,14 +231,14 @@ void test_copy_assignment() {
 
 template<typename Container>
 void test_resize_grow() {
-    Container vec(SMALL_SIZE, static_cast<typename Container::value_type>(1));
-    vec.resize(MEDIUM_SIZE, static_cast<typename Container::value_type>(2));
+    Container vec(SMALL_SIZE, typename Container::value_type(1));
+    vec.resize(MEDIUM_SIZE, typename Container::value_type(2));
     do_not_optimize(vec);
 }
 
 template<typename Container>
 void test_resize_shrink() {
-    Container vec(LARGE_SIZE, static_cast<typename Container::value_type>(1));
+    Container vec(LARGE_SIZE, typename Container::value_type(1));
     vec.resize(MEDIUM_SIZE);
     do_not_optimize(vec);
 }
@@ -167,20 +249,20 @@ void test_mixed_operations() {
     
     // Fill with initial data
     for (size_t i = 0; i < SMALL_SIZE; ++i) {
-        vec.push_back(static_cast<typename Container::value_type>(i));
+        vec.push_back(typename Container::value_type(i));
     }
     
     // Mix of operations
     for (int round = 0; round < 10; ++round) {
         // Add elements
         for (size_t i = 0; i < 100; ++i) {
-            vec.push_back(static_cast<typename Container::value_type>(i + round * 100));
+            vec.push_back(typename Container::value_type(i + round * 100));
         }
         
         // Access elements randomly
         std::mt19937 gen(42 + round);
         std::uniform_int_distribution<size_t> dis(0, vec.size() - 1);
-        volatile typename Container::value_type sum = 0;
+        volatile typename Container::value_type sum = typename Container::value_type(0);
         for (int i = 0; i < 50; ++i) {
             sum += vec[dis(gen)];
         }
@@ -248,12 +330,12 @@ void test_std_algorithm_find() {
     Container vec;
     vec.resize(MEDIUM_SIZE);
     for (size_t i = 0; i < MEDIUM_SIZE; ++i) {
-        vec[i] = static_cast<typename Container::value_type>(i);
+        vec[i] = typename Container::value_type(i);
     }
     
     volatile int found_count = 0;
     for (int target = 0; target < 1000; ++target) {
-        auto it = std::find(vec.begin(), vec.end(), static_cast<typename Container::value_type>(target));
+        auto it = std::find(vec.begin(), vec.end(), typename Container::value_type(target));
         if (it != vec.end()) {
             found_count++;
         }
@@ -266,10 +348,10 @@ void test_std_algorithm_accumulate() {
     Container vec;
     vec.resize(SMALL_SIZE);
     for (size_t i = 0; i < SMALL_SIZE; ++i) {
-        vec[i] = static_cast<typename Container::value_type>(i + 1);
+        vec[i] = typename Container::value_type(i + 1);
     }
     
-    volatile auto sum = std::accumulate(vec.begin(), vec.end(), static_cast<typename Container::value_type>(0));
+    volatile auto sum = std::accumulate(vec.begin(), vec.end(), typename Container::value_type(0));
     do_not_optimize(sum);
 }
 
@@ -279,10 +361,10 @@ void test_page_boundary_access() {
     Container vec;
     vec.resize(page_size * 4);
     for (size_t i = 0; i < page_size * 4; ++i) {
-        vec[i] = static_cast<typename Container::value_type>(i);
+        vec[i] = typename Container::value_type(i);
     }
     
-    volatile typename Container::value_type sum = 0;
+    volatile typename Container::value_type sum = typename Container::value_type(0);
     // Access elements at page boundaries
     for (size_t i = 0; i < 4; ++i) {
         size_t page_start = i * page_size;
@@ -302,131 +384,131 @@ void test_page_boundary_access() {
 
 // Push Back Performance Tests
 UBENCH(push_back_small, std_vector) {
-    test_push_back<std::vector<int>>(SMALL_SIZE);
+    test_push_back<std::vector<TestObject>>(SMALL_SIZE);
 }
 
 UBENCH(push_back_small, chunked_vector) {
-    test_push_back<chunked_vector<int>>(SMALL_SIZE);
+    test_push_back<chunked_vector<TestObject>>(SMALL_SIZE);
 }
 
 UBENCH(push_back_medium, std_vector) {
-    test_push_back<std::vector<int>>(MEDIUM_SIZE);
+    test_push_back<std::vector<TestObject>>(MEDIUM_SIZE);
 }
 
 UBENCH(push_back_medium, chunked_vector) {
-    test_push_back<chunked_vector<int>>(MEDIUM_SIZE);
+    test_push_back<chunked_vector<TestObject>>(MEDIUM_SIZE);
 }
 
 UBENCH(push_back_large, std_vector) {
-    test_push_back<std::vector<int>>(LARGE_SIZE);
+    test_push_back<std::vector<TestObject>>(LARGE_SIZE);
 }
 
 UBENCH(push_back_large, chunked_vector) {
-    test_push_back<chunked_vector<int>>(LARGE_SIZE);
+    test_push_back<chunked_vector<TestObject>>(LARGE_SIZE);
 }
 
 // Sequential Access Performance Tests
 UBENCH(sequential_access, std_vector) {
-    test_sequential_access<std::vector<int>>();
+    test_sequential_access<std::vector<TestObject>>();
 }
 
 UBENCH(sequential_access, chunked_vector) {
-    test_sequential_access<chunked_vector<int>>();
+    test_sequential_access<chunked_vector<TestObject>>();
 }
 
 // Random Access Performance Tests
 UBENCH(random_access, std_vector) {
-    test_random_access<std::vector<int>>();
+    test_random_access<std::vector<TestObject>>();
 }
 
 UBENCH(random_access, chunked_vector) {
-    test_random_access<chunked_vector<int>>();
+    test_random_access<chunked_vector<TestObject>>();
 }
 
 // Iterator Performance Tests
 UBENCH(iterator_traversal, std_vector) {
-    test_iterator_traversal<std::vector<int>>();
+    test_iterator_traversal<std::vector<TestObject>>();
 }
 
 UBENCH(iterator_traversal, chunked_vector) {
-    test_iterator_traversal<chunked_vector<int>>();
+    test_iterator_traversal<chunked_vector<TestObject>>();
 }
 
 UBENCH(range_based_loop, std_vector) {
-    test_range_based_loop<std::vector<int>>();
+    test_range_based_loop<std::vector<TestObject>>();
 }
 
 UBENCH(range_based_loop, chunked_vector) {
-    test_range_based_loop<chunked_vector<int>>();
+    test_range_based_loop<chunked_vector<TestObject>>();
 }
 
 // Memory Allocation Performance Tests
 UBENCH(reserve_performance, std_vector) {
-    test_reserve_performance<std::vector<int>>();
+    test_reserve_performance<std::vector<TestObject>>();
 }
 
 UBENCH(reserve_performance, chunked_vector) {
-    test_reserve_performance<chunked_vector<int>>();
+    test_reserve_performance<chunked_vector<TestObject>>();
 }
 
 // Construction Performance Tests
 UBENCH(construct_with_size, std_vector) {
-    test_construct_with_size<std::vector<int>>();
+    test_construct_with_size<std::vector<TestObject>>();
 }
 
 UBENCH(construct_with_size, chunked_vector) {
-    test_construct_with_size<chunked_vector<int>>();
+    test_construct_with_size<chunked_vector<TestObject>>();
 }
 
 UBENCH(construct_and_fill, std_vector) {
-    test_construct_and_fill<std::vector<int>>();
+    test_construct_and_fill<std::vector<TestObject>>();
 }
 
 UBENCH(construct_and_fill, chunked_vector) {
-    test_construct_and_fill<chunked_vector<int>>();
+    test_construct_and_fill<chunked_vector<TestObject>>();
 }
 
 // Copy Performance Tests
 UBENCH(copy_constructor, std_vector) {
-    test_copy_constructor<std::vector<int>>();
+    test_copy_constructor<std::vector<TestObject>>();
 }
 
 UBENCH(copy_constructor, chunked_vector) {
-    test_copy_constructor<chunked_vector<int>>();
+    test_copy_constructor<chunked_vector<TestObject>>();
 }
 
 UBENCH(copy_assignment, std_vector) {
-    test_copy_assignment<std::vector<int>>();
+    test_copy_assignment<std::vector<TestObject>>();
 }
 
 UBENCH(copy_assignment, chunked_vector) {
-    test_copy_assignment<chunked_vector<int>>();
+    test_copy_assignment<chunked_vector<TestObject>>();
 }
 
 // Resize Performance Tests
 UBENCH(resize_grow, std_vector) {
-    test_resize_grow<std::vector<int>>();
+    test_resize_grow<std::vector<TestObject>>();
 }
 
 UBENCH(resize_grow, chunked_vector) {
-    test_resize_grow<chunked_vector<int>>();
+    test_resize_grow<chunked_vector<TestObject>>();
 }
 
 UBENCH(resize_shrink, std_vector) {
-    test_resize_shrink<std::vector<int>>();
+    test_resize_shrink<std::vector<TestObject>>();
 }
 
 UBENCH(resize_shrink, chunked_vector) {
-    test_resize_shrink<chunked_vector<int>>();
+    test_resize_shrink<chunked_vector<TestObject>>();
 }
 
 // Mixed Operations Performance Tests
 UBENCH(mixed_operations, std_vector) {
-    test_mixed_operations<std::vector<int>>();
+    test_mixed_operations<std::vector<TestObject>>();
 }
 
 UBENCH(mixed_operations, chunked_vector) {
-    test_mixed_operations<chunked_vector<int>>();
+    test_mixed_operations<chunked_vector<TestObject>>();
 }
 
 // Large Object Performance Tests
@@ -440,28 +522,28 @@ UBENCH(large_objects, chunked_vector) {
 
 // STL Algorithm Performance Tests
 UBENCH(std_algorithm_find, std_vector) {
-    test_std_algorithm_find<std::vector<int>>();
+    test_std_algorithm_find<std::vector<TestObject>>();
 }
 
 UBENCH(std_algorithm_find, chunked_vector) {
-    test_std_algorithm_find<chunked_vector<int>>();
+    test_std_algorithm_find<chunked_vector<TestObject>>();
 }
 
 UBENCH(std_algorithm_accumulate, std_vector) {
-    test_std_algorithm_accumulate<std::vector<int>>();
+    test_std_algorithm_accumulate<std::vector<TestObject>>();
 }
 
 UBENCH(std_algorithm_accumulate, chunked_vector) {
-    test_std_algorithm_accumulate<chunked_vector<int>>();
+    test_std_algorithm_accumulate<chunked_vector<TestObject>>();
 }
 
 // Memory Pattern Tests
 UBENCH(page_boundary_access, std_vector) {
-    test_page_boundary_access<std::vector<int>>();
+    test_page_boundary_access<std::vector<TestObject>>();
 }
 
 UBENCH(page_boundary_access, chunked_vector) {
-    test_page_boundary_access<chunked_vector<int>>();
+    test_page_boundary_access<chunked_vector<TestObject>>();
 }
 
 UBENCH_MAIN(); 
